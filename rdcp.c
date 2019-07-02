576073d83ff48f17389aa10ed8dfeef2edfbf6dc
Roi Dayan <roi.dayan@gmail.com>
2019-07-02 08:46:43 +0300

    Refactor
    Move create and destroy event channel to functions.
    
    Signed-off-by: Roi Dayan <roi.dayan@gmail.com>

--- rdcp.c
+++ rdcp.c
@@ -1297,6 +1297,37 @@ free_qp:
 	return ret;
 }
 
+static int rdcp_create_event_channel(struct rdcp_cb *cb)
+{
+	int ret;
+
+	cb->cm_channel = rdma_create_event_channel();
+	if (!cb->cm_channel) {
+		perror("rdma_create_event_channel");
+		return errno;
+	}
+
+	ret = rdma_create_id(cb->cm_channel, &cb->cm_id, cb, RDMA_PS_TCP);
+	if (ret) {
+		perror("rdma_create_id");
+		goto destroy_event_chan;
+	}
+	VERBOSE_LOG(3, "created cm_id %p\n", cb->cm_id);
+
+	return 0;
+
+destroy_event_chan:
+	rdma_destroy_event_channel(cb->cm_channel);
+	return ret;
+}
+
+static void rdcp_destroy_event_channel(struct rdcp_cb *cb)
+{
+	VERBOSE_LOG(3, "destroy cm_id %p\n", cb->cm_id);
+	rdma_destroy_id(cb->cm_id);
+	rdma_destroy_event_channel(cb->cm_channel);
+}
+
 static int get_addr(char *dst, struct sockaddr *addr)
 {
 	struct addrinfo *res;
@@ -1412,19 +1443,10 @@ int main(int argc, char *argv[])
 		goto out;
 	}
 
-	cb->cm_channel = rdma_create_event_channel();
-	if (!cb->cm_channel) {
-		perror("rdma_create_event_channel");
-		ret = errno;
-		goto out;
-	}
 
-	ret = rdma_create_id(cb->cm_channel, &cb->cm_id, cb, RDMA_PS_TCP);
-	if (ret) {
-		perror("rdma_create_id");
-		goto destroy_event_chan;
-	}
-	VERBOSE_LOG(3, "created cm_id %p\n", cb->cm_id);
+	ret = rdcp_create_event_channel(cb);
+	if (ret)
+		goto out;
 
 	pthread_create(&cb->cmthread, NULL, cm_thread, cb);
 
@@ -1434,10 +1456,8 @@ int main(int argc, char *argv[])
 		ret = rdcp_run_client(cb);
 	}
 
-	VERBOSE_LOG(3, "destroy cm_id %p\n", cb->cm_id);
-	rdma_destroy_id(cb->cm_id);
-destroy_event_chan:
-	rdma_destroy_event_channel(cb->cm_channel);
+	rdcp_destroy_event_channel(cb);
+
 out:
 	free(cb);
 	return ret;
